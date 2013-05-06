636f2f67148d0aaa92dddecc35636b3f281f6bdd
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-05-06 19:06:52 -0300

    testing registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def build_resource(hash=nil)
-    if params[:user]
-      params[:user].permit!
-      self.resource = User.new(params[:user])
-      self.resource.account.name = self.resource.name
-    else
-      self.resource = User.new
-    end
+    params.require(:user).permit(:email)
+    params.require(:user).require(:account).permit(account: [:name])
+
+    self.resource = User.new params[:user]
+    self.resource.account.name = params[:user][:account][:name]
     self.resource
   end
 end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe RegistrationsController do
+  include Devise::TestHelpers
 
+  describe 'building a user' do
+    before { @request.env['devise.mapping'] = Devise.mappings[:user] }
+
+    let(:account) { stub_model(Account) }
+    let(:params)  { { user: { account: { field: :any }}} }
+    let(:user)    { stub_model(User, account: account) }
+
+    it 'sets the user account name' do
+      User.stub(new: user)
+      user.account.should_receive(:name=).with('user name')
+
+      params[:user][:account][:name] = 'user name'
+      get :new, params
+    end
+
+    it 'sets the user email' do
+      User.stub(new: user)
+      User.should_receive(:new).with(hash_including(email: 'user@example.com'))
+
+      params[:user][:email] = 'user@example.com'
+      get :new, params
+    end
+  end
 end
