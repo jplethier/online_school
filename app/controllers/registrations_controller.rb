1b94c68868c7afd0d95dfd90acf3adb122dc97fe
João Paulo Lethier <jplethier@gmail.com>
2013-05-08 14:21:10 -0300

    condicional na hora de montar o resource

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,12 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def build_resource(hash=nil)
-    params.require(:user).permit(:email)
-    params.require(:user).require(:account).permit(account: [:name])
+    if params[:user].blank?
+      self.resource = User.new
+    else
+      params.require(:user).permit(:email)
+      params.require(:user).require(:account).permit(account: [:name])
 
-    self.resource = User.new params[:user]
-    unless params[:user][:account][:name].blank?
-      self.resource.name = 'Admin ' + params[:user][:account][:name]
+      self.resource = User.new params[:user]
+      unless params[:user][:account][:name].blank?
+        self.resource.name = 'Admin ' + params[:user][:account][:name]
+      end
     end
     self.resource
   end
