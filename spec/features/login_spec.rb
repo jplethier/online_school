4e4ef17ca70779f6c529e2c98fcce95b9c76de67
João Paulo Lethier <jplethier@gmail.com>
2013-05-08 16:19:53 -0300

    teste para login funcionando

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,22 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe "Login" do
+
+  let(:user) { FactoryGirl.create(:user, password: '123qwe', password_confirmation: '123qwe') }
+
+  subject { page }
+
+  before do
+    visit new_user_session_path
+  end
+
+  describe 'creating a user' do
+    it 'successfully' do
+      fill_in 'user_email', :with => user.email
+      fill_in 'user_password', :with => '123qwe'
+      click_on 'signin_btn'
+      should have_content('Login realizado com sucesso.')
+    end
+  end
+end
