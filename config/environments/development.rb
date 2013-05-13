25f5ef28de9c681b6b7327a1b8f7efe35128e8ef
João Paulo Lethier <jplethier@gmail.com>
2013-05-13 19:03:31 -0300

    configurando para usar o letter_opener em development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,9 @@ OnlineSchool::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
