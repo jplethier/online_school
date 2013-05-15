00ab3ada11fe2932326d42d69240c5a99944249a
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-05-15 13:17:18 -0300

    por padrao os papeis do usuario sao todos falsos no banco de dados

--- db/migrate/20130509014904_add_boolean_fields_to_users.rb
+++ db/migrate/20130509014904_add_boolean_fields_to_users.rb
@@ -1,8 +1,8 @@
 class AddBooleanFieldsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :student, :boolean
-    add_column :users, :teacher, :boolean
-    add_column :users, :employee, :boolean
-    add_column :users, :admin, :boolean
+    add_column :users, :student,  :boolean, default: false
+    add_column :users, :teacher,  :boolean, default: false
+    add_column :users, :employee, :boolean, default: false
+    add_column :users, :admin,    :boolean, default: false
   end
 end
