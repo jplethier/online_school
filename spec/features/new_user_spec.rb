ccc195be176c357b8d509dab6656b76b9f4110e7
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-05-23 18:43:40 -0300

    new user page object

--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -1,45 +1,41 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-describe "New User" do
-
-  subject { page }
-
-  before do
-    visit new_user_registration_path
-  end
-
+describe 'New User' do
   describe 'creating a user' do
+    let(:new_user_page) { NewUser.visit }
+
     it 'successfully' do
-      count = User.count
-      a_count = Account.count
-      within 'article' do
-        fill_in 'user_account_attributes_name', :with => 'Nome da Escola'
-        fill_in 'user_account_attributes_subdomain', :with => 'escola1'
-        fill_in 'user_email', :with => 'user@school.com.be'
-        fill_in 'user_password', :with => '123qwe'
-        fill_in 'user_password_confirmation', :with => '123qwe'
-        click_on 'signup_btn'
-      end
-      (User.count - 1).should == count
-      (Account.count - 1).should == a_count
-      should have_content('Uma mensagem com um link de confirmação foi enviada para o seu endereço de e-mail. Por favor, abra o link para confirmar a sua conta.')
+      user_count    = User.count
+      account_count = Account.count
+
+      new_user_page.account_name          = 'Nome da escola'
+      new_user_page.account_subdomain     = 'subdominioescola'
+      new_user_page.email                 = 'user@example.com'
+      new_user_page.password              = '1234qwer'
+      new_user_page.password_confirmation = '1234qwer'
+
+      new_user_page.create
+
+      expect(User.count).to     eq user_count + 1
+      expect(Account.count).to  eq account_count + 1
+      expect(new_user_page).to have_content 'Uma mensagem com um link de confirmação foi enviada para o seu endereço de e-mail. Por favor, abra o link para confirmar a sua conta.'
     end
 
     it 'unsuccessfully' do
-      count = User.count
-      a_count = Account.count
-      within 'article' do
-        fill_in 'user_account_attributes_name', :with => 'Nome da Escola'
-        fill_in 'user_account_attributes_subdomain', :with => 'escola1'
-        # fill_in 'user_email', :with => 'user@school.com.be'
-        fill_in 'user_password', :with => '123qwe'
-        fill_in 'user_password_confirmation', :with => '123qwe'
-        click_on 'signup_btn'
-      end
-      (User.count).should == count
-      (Account.count).should == a_count
-      should have_content('Não foi possível salvar')
+      user_count    = User.count
+      account_count = Account.count
+
+      new_user_page.account_name          = 'Nome da escola'
+      new_user_page.account_subdomain     = 'subdominioescola'
+      new_user_page.password              = '1234qwer'
+      new_user_page.password_confirmation = '1234qwer'
+
+      new_user_page.create
+
+      expect(User.count).to     eq user_count
+      expect(Account.count).to  eq account_count
+      expect(new_user_page).to have_content 'Não foi possível salvar'
     end
   end
 end
--- spec/pages/new_user.rb
+++ spec/pages/new_user.rb
@@ -1,28 +1,31 @@
-class NewStudent < SitePrism::Page
-  set_url "http://{subdomain}.lvh.me:3000/students/new"
+class NewUser < SitePrism::Page
+  set_url "/users/sign_up"
 
-  element :email,                 "input[name='user[email]']"
-  element :name,                  "input[name='user[name]']"
-  element :password,              "input[name='user[password]']"
-  element :password_confirmation, "input[name='user[password_confirmation]']"
-  element :create_button,         "input[name='commit'].primary"
+  element :account_name,          "article input[name='user[account_attributes][name]']"
+  element :account_subdomain,     "article input[name='user[account_attributes][subdomain]']"
+  element :email,                 "article input[name='user[email]']"
+  element :password,              "article input[name='user[password]']"
+  element :password_confirmation, "article input[name='user[password_confirmation]']"
+  element :create_button,         "article input[name='commit']"
 
-  def self.visit(hash)
-    page = self.new
-    page.load(hash)
-    page
+  def self.visit
+    self.new.tap { |page| page.load }
   end
 
   def create
     self.create_button.click
   end
 
-  def email=(email)
-    self.email.set email
+  def account_name=(name)
+    self.account_name.set name
   end
 
-  def name=(name)
-    self.name.set name
+  def account_subdomain=(subdomain)
+    self.account_subdomain.set subdomain
+  end
+
+  def email=(email)
+    self.email.set email
   end
 
   def password=(password)
