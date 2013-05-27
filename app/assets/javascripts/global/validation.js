a8eb917383eb91b1b43f8f44d7517291c284dab7
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-05-27 20:34:32 -0300

    adicionando mais traducoes para o jquery validation

--- app/assets/javascripts/global/validation.js
+++ app/assets/javascripts/global/validation.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
   $.extend($.validator.messages, {
-    email: 'Informe um email válido',
-    required: 'Campo obrigatório',
-    url: 'Please enter a valid URL.',
-    maxlength: $.validator.format('Please enter no more than {0} characters.'),
-    minlength: $.validator.format('Please enter at least {0} characters.'),
+    required: "Campo obrigat&oacute;rio.",
+    remote: "Por favor, corrija este campo.",
+    email: 'Informe um email v&aacute;lido',
+    url: "Informe uma URL v&aacute;lida.",
+    date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
+    dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
+    number: "Por favor, forne&ccedil;a um n&uacute;mero v&aacute;lido.",
+    digits: "Por favor, forne&ccedil;a somente d&iacute;gitos.",
+    creditcard: "Por favor, forne&ccedil;a um cart&atilde;o de cr&eacute;dito v&aacute;lido.",
+    equalTo: "Informe o mesmo valor novamente.",
+    accept: "Por favor, forne&ccedil;a um valor com uma extens&atilde;o v&aacute;lida.",
+    maxlength: $.validator.format("Por favor, forne&ccedil;a n&atilde;o mais que {0} caracteres."),
+    minlength: $.validator.format("Por favor, forne&ccedil;a ao menos {0} caracteres."),
+    rangelength: $.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1} caracteres de comprimento."),
+    range: $.validator.format("Por favor, forne&ccedil;a um valor entre {0} e {1}."),
+    max: $.validator.format("Por favor, forne&ccedil;a um valor menor ou igual a {0}."),
+    min: $.validator.format("Por favor, forne&ccedil;a um valor maior ou igual a {0}.")
   });
 });
 
