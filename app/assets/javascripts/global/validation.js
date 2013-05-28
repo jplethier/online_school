d78b7869a100884fb8d9f2b8de2ea0afecd32019
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-05-28 00:07:23 -0300

    layout do formulario de cadastro quando tem erro

--- app/assets/javascripts/global/validation.js
+++ app/assets/javascripts/global/validation.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+  $.validator.setDefaults({
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+      $(element).parent().addClass(errorClass);
+      $(element).parent().parent().addClass(errorClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+      $(element).parent().removeClass(errorClass);
+
+      if ($(element).parent().siblings('.error').length === 0) {
+        $(element).parent().parent().removeClass(errorClass);
+      }
+    }
+  });
+
   $.extend($.validator.messages, {
     required: "Campo obrigat&oacute;rio.",
     remote: "Por favor, corrija este campo.",
