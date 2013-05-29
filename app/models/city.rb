6d3e17b8d8b718053d0ec53cde63bd7a6942d0de
João Paulo Lethier <jplethier@gmail.com>
2013-05-28 22:44:35 -0300

    annotate city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  uf         :string(2)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class City < ActiveRecord::Base
   validates :name, presence: true
   validates :uf,   presence: true
