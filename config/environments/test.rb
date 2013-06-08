597ea94323346d17b0f1472c3e41194266df2e62
João Paulo Lethier <jplethier@gmail.com>
2013-06-07 22:19:26 -0300

    fix tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,7 +2,7 @@ OnlineSchool::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   #aws
-  AWS_CONFIG = YAML.load_file("#{Rails.root}/config/aws.yml")['development']
+  AWS_CONFIG = YAML.load_file("#{Rails.root}/config/aws.yml")['test']
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
