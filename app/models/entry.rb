27fe06f3ccffe1bf0fb53652fdf05d1429e0a539
João Paulo Lethier <jplethier@gmail.com>
2013-07-31 16:59:43 -0300

    annotate

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: entries
+#
+#  id            :integer          not null, primary key
+#  resource_id   :integer
+#  resource_type :string(255)
+#  classroom_id  :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Entry < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :resource, polymorphic: true
