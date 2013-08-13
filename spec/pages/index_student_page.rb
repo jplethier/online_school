ae34da473c96b36380579823f58a32414a730b7e
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-08-13 10:44:18 -0300

    extracting search partial from students

--- spec/pages/index_student_page.rb
+++ spec/pages/index_student_page.rb
@@ -4,8 +4,8 @@ end
 class IndexStudentPage < Page
   set_url "http://{subdomain}.lvh.me:31234/students"
 
-  element :search_field,  "#student_search input[name='search']"
-  element :search_button, "#student_search button[type='submit']"
+  element :search_field,  ".search-wrapper input[name='search']"
+  element :search_button, ".search-wrapper button[type='submit']"
 
   sections :students, StudentSection, '#students li'
 
