c68f7f633bf3c40180a298d2485c0c3192d6f9e9
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-08-15 19:02:55 -0300

    clickable tr: redirect to group show page

--- app/assets/javascripts/global/autoload.js
+++ app/assets/javascripts/global/autoload.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $('form[data-validate]').validate();
 
   $('*[data-selectize]').selectize();
+
+  $('tr[data-url]').each(function() {
+    $(this).on('click', function() {
+      window.location.href = $(this).data('url');
+    });
+  });
 });
