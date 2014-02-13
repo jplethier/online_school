c4e97a7302935436edb798add1df0ea41fc8abbc
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-02-13 19:45:10 -0200

    parsing temporary group ids parameters

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -3,11 +3,12 @@ class ClassroomsController < AuthorizedController
   before_filter :populate_groups_subjects_and_teachers, only: [:new, :edit]
   before_filter :populate_classroom_groups, only: [:edit]
 
-  prepend_before_filter :create_subject_when_subject_id_is_a_string, only: [:create, :update]
-
   prepend_before_filter do
     params[:classroom] &&= classroom_params
   end
+
+  prepend_before_filter :create_subject_when_subject_id_is_a_string, only: [:create, :update]
+  prepend_before_filter :parse_group_ids, only: [:create, :update]
 
   def index
     if @classrooms.blank?
@@ -43,11 +44,11 @@ class ClassroomsController < AuthorizedController
   private
 
   def classroom_params
-    params.require(:classroom).permit(:subject_id, :teacher_id, entries_attributes: [:id, :resource_id, :_destroy])
+    params.require(:classroom).permit(:subject_id, :teacher_id, temporary_group_ids: [])
   end
 
   def populate_classroom_groups
-    @classroom_groups = @classroom.groups.where(id: @classroom.group_ids)
+    @classroom_groups = @classroom.groups
   end
 
   def populate_groups_subjects_and_teachers
@@ -56,18 +57,17 @@ class ClassroomsController < AuthorizedController
     @teachers = current_account.users.teachers
   end
 
-  #def assign_all_students_of_a_group
-    #if classroom = params[:classroom]
-      #classroom[:entries_attributes].values.each do |entry|
-        #group = Group.find entry.delete(:resource_id)
-        #group.students.map(&:user_id)
-      #end
-    #end
-  #end
-
   def create_subject_when_subject_id_is_a_string
     unless params[:classroom][:subject_id].is_number?
       params[:classroom][:subject_id] = current_account.subjects.find_or_create_by(name: params[:classroom][:subject_id]).id
+    end
+  end
+
+  def parse_group_ids
+    if params[:classroom][:temporary_group_ids]
+      params[:classroom][:temporary_group_ids] = params[:classroom][:temporary_group_ids].map(&:to_i)
+    else
+      params[:classroom][:temporary_group_ids] = []
     end
   end
 end
