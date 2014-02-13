e004ce79547e3662ab8cf1511289ca8e4fb6010b
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-02-13 19:36:20 -0200

    creating and removing entries when adding and removing groups to classroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,22 +5,50 @@ class Classroom < ActiveRecord::Base
 
   has_many :entries, inverse_of: :classroom
   has_many :user_groups, through: :entries
-  has_many :users, through: :user_groups, source: :user
+  has_many :users,  through: :user_groups, source: :user
   has_many :groups, through: :user_groups, source: :group
 
   has_many :exams
-
   has_many :topics
 
   validates :account, presence: true
   validates :subject, presence: true
+
+  after_save :update_groups
 
   scope :ordered_by_name, lambda { joins{ subject }.order{ subject.name } }
   scope :search, lambda { |query| joins{ subject }.where{subject.name =~ "%#{query.gsub(' ','%')}%"} }
 
   accepts_nested_attributes_for :entries, allow_destroy: true
 
+  attr_accessor :temporary_group_ids
+
+  def temporary_group_ids
+    @temporary_group_ids ||= []
+  end
+
   def groups_names
-    self.groups.map(&:name).join(', ')
+    self.groups.pluck(:name).join(', ')
+  end
+
+  private
+
+  def update_groups
+    create_new_groups_entries
+    remove_deleted_groups_entries
+  end
+
+  def create_new_groups_entries
+    temporary_group_ids.each do |group_id|
+      UserGroup.where(group_id: group_id).each do |user_group|
+        user_group.entries.create(classroom: self)
+      end
+    end
+  end
+
+  def remove_deleted_groups_entries
+    (group_ids - temporary_group_ids).each do |group_id|
+      Entry.includes(:user_group).destroy_all(classroom_id: id, user_group: { group_id: group_id })
+    end
   end
 end
--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -16,4 +16,44 @@ describe Classroom do
       expect{ classroom.account = nil }.to change{ classroom.valid? }.from(true).to(false)
     end
   end
+
+  describe 'when adding groups' do
+    let(:user_groups) { FactoryGirl.create_list(:user_group, 2) }
+
+    it 'new entries are created' do
+      classroom.temporary_group_ids = user_groups.map(&:id)
+
+      expect { classroom.save! }.to change { classroom.entries.count }.from(0).to(user_groups.count)
+    end
+
+    it 'existing entries remain untouched' do
+      FactoryGirl.create(:entry, classroom: classroom, user_group: user_groups.first)
+
+      classroom.temporary_group_ids = user_groups.map(&:id)
+      expect { classroom.save! }.to change { classroom.entries.count }.from(1).to(user_groups.count)
+    end
+  end
+
+  describe 'when deleting groups' do
+    let(:user_groups) { FactoryGirl.create_list(:user_group, 1) }
+
+    it 'entries are removed' do
+      FactoryGirl.create(:entry, classroom: classroom, user_group: user_groups.first)
+
+      classroom.temporary_group_ids = []
+      expect { classroom.save! }.to change { classroom.entries.count }.from(1).to(0)
+    end
+  end
+
+  describe 'when adding and deleting groups' do
+    let(:user_groups) { FactoryGirl.create_list(:user_group, 2) }
+
+    it 'only deleted group entries are removed' do
+      FactoryGirl.create(:entry, classroom: classroom, user_group: user_groups.first)
+      FactoryGirl.create(:entry, classroom: classroom, user_group: user_groups.last)
+
+      classroom.temporary_group_ids = [user_groups.first.group_id]
+      expect { classroom.save! }.to change { classroom.entries.count }.from(2).to(1)
+    end
+  end
 end
